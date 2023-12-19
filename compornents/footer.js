66740856f9fb1895b4f67f9ae67e80ce0c33ac1b
hiag yuta <s23022@std.it-college.ac.jp>
2023-12-19 07:35:43 +0000

    p108 p110 finished
    ヒーロのレイアウトを整える、フッターのレイアウトを整える chapter4.3 4.4

--- compornents/footer.js
+++ compornents/footer.js
@@ -1,8 +1,11 @@
 import Logo from 'compornents/logo'
+import styles from 'styles/footer.module.css'
 
 const Footer = () => (
-  <footer>
-    <Logo />
+  <footer className={styles.wrapper}>
+    <div className={styles.flexContainer}>
+      <Logo />
+    </div>
   </footer>
 )
 export default Footer
