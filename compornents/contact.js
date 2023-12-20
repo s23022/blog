9470544f8916581c6291bd99a35eb1030f255294
hiag yuta <s23022@std.it-college.ac.jp>
2023-12-20 07:54:10 +0000

    p182 finished
    アイコンのサイズ（フォントサイズ）をpropsで指定できるようにする
    chapter5.10 終了

--- compornents/contact.js
+++ compornents/contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div className={styles.stack}>
       <h3 className={styles.heading}>Contact</h3>
-      <Social />
+      <Social iconSize='30px' />
       <address>cube@web.mail.address</address>
     </div>
   )
--- compornents/social.js
+++ compornents/social.js
@@ -7,9 +7,9 @@ import {
   faGithub
 } from '@fortawesome/free-brands-svg-icons'
 
-const Social = () => {
+const Social = ({ iconSize = 'initial' }) => {
   return (
-    <ul className={styles.list}>
+    <ul className={styles.list} style={{ '--icon-size': iconSize }}>
       <li>
         <a href='https://twitter.com/'>
           <FontAwesomeIcon icon={faTwitter} />
