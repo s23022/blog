28b026de6c6a741042669d1fee3cd75777bf01b3
hiag yuta <s23022@std.it-college.ac.jp>
2023-12-20 00:47:12 +0000

    p125 finished
    Contactコンポーネントでコンタクト情報を管理する chapter4.7 終了

---
+++ compornents/contact.js
@@ -0,0 +1,11 @@
+import styles from 'styles/contact.module.css'
+
+const Contact = () => {
+  return (
+    <div className={styles.stack}>
+      <h3 className={styles.heading}>Contact</h3>
+      <address>cube@web.mail.address</address>
+    </div>
+  )
+}
+export default Contact
