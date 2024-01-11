cbe07fce022a7394b302cd117b11f30c6ab1366e
hiag yuta <s23022@std.it-college.ac.jp>
2024-01-11 08:25:04 +0000

    p268 9.5 記事一覧を作成する
    教科書通り進んだ

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,27 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'compornents/meta'
 import Container from 'compornents/container'
 import Hero from 'compornents/hero'
+import Posts from 'compornents/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
+
+      <Posts posts={posts} />
     </Container>
   )
+}
+
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
 }
 export default Blog
