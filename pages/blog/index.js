b9caa477c453e6e7f1e9c12d982c24b5f4183d58
hiag yuta <s23022@std.it-college.ac.jp>
2024-01-11 09:45:28 +0000

    p272 9.6 記事一覧にnext/imageでアイキャッチ画像を追加する
    p273 の
    if(!post.hasOwnProperty('eyecatch'))の部分でエラーが起き、調べて修正し実行はできたが、表示される画像がすべてeyecatch.jpgの画像で表示され、解決できないまま終わった

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,10 @@ import Meta from 'compornents/meta'
 import Container from 'compornents/container'
 import Hero from 'compornents/hero'
 import Posts from 'compornents/posts'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -17,6 +21,14 @@ const Blog = ({ posts }) => {
 
 export async function getStaticProps () {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty.call('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
