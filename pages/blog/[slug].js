9f094b15a92daf60a659bad5bcd5d21eb3b2ba8b
hiag yuta <s23022@std.it-college.ac.jp>
2024-01-11 05:51:33 +0000

    p252 9.2 Dynamic Routesで記事のページを生成する
    教科書通り進んだ schedule.jsを[slug].jsに変更した

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -17,7 +17,7 @@ import { getPlaiceholder } from 'plaiceholder'
 // ローカルの代替アイキャッチ画像
 import { eyecatchLocal } from 'lib/constants'
 
-export default function Schedule ({
+export default function Post ({
   title,
   publish,
   content,
@@ -66,8 +66,15 @@ export default function Schedule ({
   )
 }
 
-export async function getStaticProps () {
-  const slug = 'micro'
+export async function getStaticPaths () {
+  return {
+    paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+    fallback: false
+  }
+}
+
+export async function getStaticProps (context) {
+  const slug = context.params.slug
 
   const post = await getPostBySlug(slug)
 
