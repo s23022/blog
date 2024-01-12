bf113c9017ff98edd3a9b264f8228c98175f25ec
hiag yuta <s23022@std.it-college.ac.jp>
2024-01-12 05:46:15 +0000

    p276 9.7 トップページに記事一覧を表示する
    教科書通り進んだ

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,42 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'compornents/meta'
 import Container from 'compornents/container'
 import Hero from 'compornents/hero'
+import Posts from 'compornents/posts'
+import Pagination from 'compornents/pagination'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Home = () => {
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+const Home = ({ posts }) => {
   return (
     <Container>
       <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
+}
+
+export async function getStaticProps () {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
 }
 
 export default Home
