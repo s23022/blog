1d6cc8ae18fd034a2acce2adef8aa16e51baf2ad
hiag yuta <s23022@std.it-college.ac.jp>
2024-01-16 08:55:59 +0000

    p311 10.5 useStateとuseRefを使ってアコーディオンを作成する
    教科書通り進んだ

--- pages/about.js
+++ pages/about.js
@@ -3,14 +3,14 @@ import Container from 'compornents/container'
 import Hero from 'compornents/hero'
 import PostBody from 'compornents/post-body'
 import Contact from 'compornents/contact'
-import Image from 'next/image'
-import eyecatch from 'images/about.jpg'
-
 import {
   TwoColumn,
   TwoColumnMain,
   TwoColumnSidebar
 } from 'compornents/two-column'
+import Accordion from 'compornents/accordion'
+import Image from 'next/image'
+import eyecatch from 'images/about.jpg'
 
 const About = () => {
   return (
@@ -54,15 +54,21 @@ const About = () => {
             </p>
 
             <h2>FAQ</h2>
-            <p>
-              プログラミングのポイントは、作りたいものを作ることです。楽しいことから思いつき、目標とゴールを決め、そこに向かってさまざまな課題を設定していきながら、プログラムを作っていきます。
-            </p>
-            <p>
-              古代語を解読するのに必要なのは、書かれた文字そのものだけです。古代の世界観や思考方法。それらを読み取ってこそ古代の世界観が理解できてきます。
-            </p>
-            <p>
-              公開リポジトリを活用すると、全世界のどこからでもアクセスし、開発者が関連するプロジェクトのタスクを利用することができます。
-            </p>
+            <Accordion heading='プログラミングのポイントについて'>
+              <p>
+                プログラミングのポイントは、作りたいものを作ることです。楽しいことから思いつき、目標とゴールを決め、そこに向かってさまざまな課題を設定していきながら、プログラムを作っていきます。
+              </p>
+            </Accordion>
+            <Accordion heading='古代語の解読について'>
+              <p>
+                古代語を解読するのに必要なのは、書かれた文字そのものだけです。古代の世界観や思考方法。それらを読み取ってこそ古代の世界観が理解できてきます。
+              </p>
+            </Accordion>
+            <Accordion heading='公開リポジトリの活用について'>
+              <p>
+                公開リポジトリを活用すると、全世界のどこからでもアクセスし、開発者が関連するプロジェクトのタスクを利用することができます。
+              </p>
+            </Accordion>
           </PostBody>
         </TwoColumnMain>
 
